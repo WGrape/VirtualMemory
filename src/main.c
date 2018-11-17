406f36a52c49f15e82b8dfb2b5f09e2ceeab0f22
PC\Administrator <15239928381@163.com>
2018-11-17 20:35:45 +0800

    no message

--- src/main.c
+++ src/main.c
@@ -3,23 +3,35 @@
 int main(){
 
 	int type;
+	Memory memory;
 
 	while(1){
+
+		// 分配内存
+		memory.pr = alloc_memory();
 
 		// 选择启动不同的虚拟存储器
 		printf("Choose the different types of virtual memory\n1(Page) 2(Segment) 3(SegmentPage) : ");
 		scanf("%d",&type);
 
-		Memory memory = {alloc_memory()};
-
 		if(type==1){
 
+			// 页式虚拟存储器
+			page_virtual_memory_init();
 
+		}else if(type==2){
+
+			// 段式虚拟存储器
+			segment_virtual_memory_init();
+
+		}else{
+
+			// 段页式虚拟存储器
+			segment_page_virtual_memory_init();
 		}
 
-
-		collec_memory(memory.p);
-
+		// 回收内存
+		collec_memory(memory.pr);
 	}
 
 	return 0;
--- src/virtual/page/module/init/init.c
+++ src/virtual/page/module/init/init.c
@@ -1,2 +1,11 @@
 #include<stdio.h>
 
+void page_virtual_memory_init(){
+
+	printf("Page Virtual Memory Init Successfully !");
+	while(1){
+
+
+
+	}
+}
